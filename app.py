e8ba0a06347b42fd9b55c9607bc2f91df44f265d
Glowstone752 <thawornbutjirapat@gmail.com>
2024-07-03 11:51:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,12 @@ def traffic_thailand():
     q = "thailand traffic"
     r = palm.chat(**model, messages=q)
     return(render_template("traffic_thailand.html",r=r.last))
-
+    
+@app.route("/traffic_singapore", methods=["GET","POST"])
+def traffic_thailand():
+    q = "traffic_singapore"
+    r = palm.chat(**model, messages=q)
+    return(render_template("traffic_singapore.html",r=r.last))
+    
 if __name__ == "__main__":
     app.run()
